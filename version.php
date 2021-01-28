c3c204175e119e0aba81e486275be8ef785ed17c
hamboneZA <31657055+hamboneZA@users.noreply.github.com>
2021-01-29 01:15:23 +0200

    Update version.php

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
+/**BUMP
  * MOODLE VERSION INFORMATION
  *
  * This file defines the current version of the core Moodle code being used.
